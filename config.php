c5157d28cce33e8e3b0ec1e6d136dda3ee2202c3
salas-steren <109772576+Admin-Salas2022@users.noreply.github.com>
2022-07-25 17:46:14 -0500

    Update and rename config-sample.php to config.php

--- config.php
+++ config.php
@@ -30,7 +30,7 @@ class Config {
     // GENERAL SETTINGS
     // ------------------------------------------------------------------------
 
-    const BASE_URL      = 'http://url-to-easyappointments-directory';
+    const BASE_URL      = 'https://github.com/Admin-Salas2022/salas/edit/main/';
     const LANGUAGE      = 'english';
     const DEBUG_MODE    = FALSE;
 
