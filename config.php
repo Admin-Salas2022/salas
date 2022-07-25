f66e1e785305a71608d21fa1e3a077b8ecb41eca
salas-steren <109772576+Admin-Salas2022@users.noreply.github.com>
2022-07-25 17:48:27 -0500

    Update config.php

--- config.php
+++ config.php
@@ -30,7 +30,7 @@ class Config {
     // GENERAL SETTINGS
     // ------------------------------------------------------------------------
 
-    const BASE_URL      = 'https://github.com/Admin-Salas2022/salas/edit/main/';
+    const BASE_URL      = 'https://github.com/Admin-Salas2022/salas/find/main';
     const LANGUAGE      = 'english';
     const DEBUG_MODE    = FALSE;
 
